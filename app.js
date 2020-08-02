0358b0094902f8f3f1a5d88bc429a1822271e05b
ThePsyk3y <50011738+ThePsyk3y@users.noreply.github.com>
2020-08-02 12:57:45 +0530

    Functionality Changes.
    Added Addition Functionality to application.
    
    Added Result Showing button function.
    
    Added a DRY clear number-view function.
    
    Made getting the document of "digits" easier with a variable.
    
    Additon of debugging logs to console.

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
+/* eslint-disable no-console */
+//  *Variable Declarations
+let prevNum;
+let currNum;
+let currOp;
+const getDigDoc = document.getElementById('digits');
+
+function clearDisp() {
+  getDigDoc.textContent = 0;
+}
+
 // *Number Button on-click function
 function numberDisp(num) {
   return function () {
-    document.getElementById('digits').textContent = document.getElementById('digits').textContent * 10 + num;
+    getDigDoc.textContent = getDigDoc.textContent * 10 + num;
+    currNum = parseInt(getDigDoc.textContent, 10);
+    console.log(currNum);
   };
 }
 
@@ -17,6 +30,23 @@ const dispNum7 = numberDisp(7);
 const dispNum8 = numberDisp(8);
 const dispNum9 = numberDisp(9);
 
+function numberAdd() {
+  prevNum = currNum;
+  console.log(prevNum);
+  currOp = '+';
+  clearDisp();
+}
+
+function resDisp() {
+  getDigDoc.textContent = 0;
+  if (currOp === '+') {
+    const sum = prevNum + currNum;
+    console.log(sum);
+    getDigDoc.textContent = sum;
+    currNum = parseInt(getDigDoc.textContent, 10);
+  }
+}
+
 //  *Button click Behaviour
 // ?For Number Button Click
 document.getElementById('btn-0').addEventListener('click', dispNum0);
@@ -29,3 +59,7 @@ document.getElementById('btn-6').addEventListener('click', dispNum6);
 document.getElementById('btn-7').addEventListener('click', dispNum7);
 document.getElementById('btn-8').addEventListener('click', dispNum8);
 document.getElementById('btn-9').addEventListener('click', dispNum9);
+
+//  ?For Operator Button Click
+document.getElementById('btn-add').addEventListener('click', numberAdd);
+document.getElementById('btn-res').addEventListener('click', resDisp);
