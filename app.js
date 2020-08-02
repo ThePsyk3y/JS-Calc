c72c31fc737c6430d25822f024b03a33b1e80835
ThePsyk3y <50011738+ThePsyk3y@users.noreply.github.com>
2020-08-02 06:21:22 +0530

    Prototype Script File Created.
    Added Behaviours to all digit buttons.
    (WARNING HIGHLY INEFFICIENT!)
    
    (TEMP) Clicking buttons just adds them to the number-view.

---
+++ app.js
@@ -0,0 +1,41 @@
+function addNum0() {
+  document.getElementById('digits').textContent = document.getElementById('digits').textContent * 10 + 0;
+}
+function addNum1() {
+  document.getElementById('digits').textContent = document.getElementById('digits').textContent * 10 + 1;
+}
+function addNum2() {
+  document.getElementById('digits').textContent = document.getElementById('digits').textContent * 10 + 2;
+}
+function addNum3() {
+  document.getElementById('digits').textContent = document.getElementById('digits').textContent * 10 + 3;
+}
+function addNum4() {
+  document.getElementById('digits').textContent = document.getElementById('digits').textContent * 10 + 4;
+}
+function addNum5() {
+  document.getElementById('digits').textContent = document.getElementById('digits').textContent * 10 + 5;
+}
+function addNum6() {
+  document.getElementById('digits').textContent = document.getElementById('digits').textContent * 10 + 6;
+}
+function addNum7() {
+  document.getElementById('digits').textContent = document.getElementById('digits').textContent * 10 + 7;
+}
+function addNum8() {
+  document.getElementById('digits').textContent = document.getElementById('digits').textContent * 10 + 8;
+}
+function addNum9() {
+  document.getElementById('digits').textContent = document.getElementById('digits').textContent * 10 + 9;
+}
+
+document.getElementById('btn-0').addEventListener('click', addNum0);
+document.getElementById('btn-1').addEventListener('click', addNum1);
+document.getElementById('btn-2').addEventListener('click', addNum2);
+document.getElementById('btn-3').addEventListener('click', addNum3);
+document.getElementById('btn-4').addEventListener('click', addNum4);
+document.getElementById('btn-5').addEventListener('click', addNum5);
+document.getElementById('btn-6').addEventListener('click', addNum6);
+document.getElementById('btn-7').addEventListener('click', addNum7);
+document.getElementById('btn-8').addEventListener('click', addNum8);
+document.getElementById('btn-9').addEventListener('click', addNum9);
