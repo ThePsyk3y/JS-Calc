7128772b8ab1231e4132e461525fa0664763b4f4
ThePsyk3y <50011738+ThePsyk3y@users.noreply.github.com>
2020-08-08 03:47:00 +0530

    Major Number Display and Calculation Changes.
    Replaced every getDigDoc textcontent with value.
    
    No Longer needs 10 button call event listeners.
    
    Single query selected Event Listener calls number display function.
    
    Fixed Digit count bug which interferred with global current number.

--- js/app.js
+++ js/app.js
@@ -11,10 +11,10 @@ let opCount = 0;
 const getDigDoc = document.getElementById('digits');
 
 function clearDisp() {
-  getDigDoc.textContent = 0;
+  getDigDoc.value = '';
 }
 function fullClearDisp() {
-  getDigDoc.textContent = 0;
+  getDigDoc.value = '';
   prevNum = 0;
   currNum = 0;
   digiCount = 1;
@@ -23,8 +23,9 @@ function fullClearDisp() {
 
 function digitCount() {
   digiCount = 1;
-  while (currNum / 10 >= 1) {
-    currNum /= 10;
+  let currcop = currNum;
+  while (currcop / 10 >= 1) {
+    currcop /= 10;
     digiCount++;
   }
   console.log(digiCount);
@@ -32,45 +33,46 @@ function digitCount() {
 
 function resDisp() {
   opCount = 0;
-  getDigDoc.textContent = 0;
+  getDigDoc.value = '';
   if (currOp === '+') {
     console.log(prevNum + currNum);
-    getDigDoc.textContent = prevNum + currNum;
-    currNum = parseInt(getDigDoc.textContent, 10);
+    getDigDoc.value = prevNum + currNum;
+    currNum = parseInt(getDigDoc.value, 10);
   }
   if (currOp === '-') {
     console.log(prevNum - currNum);
-    getDigDoc.textContent = prevNum - currNum;
-    currNum = parseInt(getDigDoc.textContent, 10);
+    getDigDoc.value = prevNum - currNum;
+    currNum = parseInt(getDigDoc.value, 10);
   }
   if (currOp === '*') {
     console.log(prevNum * currNum);
-    getDigDoc.textContent = prevNum * currNum;
-    currNum = parseInt(getDigDoc.textContent, 10);
+    getDigDoc.value = prevNum * currNum;
+    currNum = parseInt(getDigDoc.value, 10);
   }
   if (currOp === '/') {
     console.log(prevNum / currNum);
-    getDigDoc.textContent = prevNum / currNum;
-    currNum = parseInt(getDigDoc.textContent, 10);
+    getDigDoc.value = prevNum / currNum;
+    currNum = parseInt(getDigDoc.value, 10);
   }
 }
 
 // *Number Button on-click function
 function numberDisp(num) {
-  return function () {
-    if (currNum >= 0) {
-      getDigDoc.textContent = getDigDoc.textContent * 10 + num;
-      currNum = parseInt(getDigDoc.textContent, 10);
-      console.log(currNum);
-    } else {
-      getDigDoc.textContent = getDigDoc.textContent * 10 - num;
-      currNum = parseInt(getDigDoc.textContent, 10);
-      console.log(currNum);
-    }
-    digitCount();
-  };
+  if (currNum >= 0) {
+    // getDigDoc.textContent = getDigDoc.value * 10 + num;
+    getDigDoc.value = getDigDoc.value === '0' ? currNum : getDigDoc.value + num;
+    currNum = parseInt(getDigDoc.value, 10);
+    console.log(currNum);
+  } else {
+    // getDigDoc.textContent = getDigDoc.value * 10 - num;
+    getDigDoc.value = getDigDoc.value === '0' ? currNum * -1 : getDigDoc.value - num;
+    currNum = parseInt(getDigDoc.value, 10);
+    console.log(currNum);
+  }
+  digitCount();
 }
 
+/*
 const dispNum = [
   numberDisp(0),
   numberDisp(1),
@@ -83,6 +85,7 @@ const dispNum = [
   numberDisp(8),
   numberDisp(9),
 ];
+*/
 
 function operations(oper) {
   return function () {
@@ -137,16 +140,21 @@ function flipNum() {
 
 //  *Button click Behaviour
 // ?For Number Button Click
-document.getElementById('btn-0').addEventListener('click', dispNum[0]);
-document.getElementById('btn-1').addEventListener('click', dispNum[1]);
-document.getElementById('btn-2').addEventListener('click', dispNum[2]);
-document.getElementById('btn-3').addEventListener('click', dispNum[3]);
-document.getElementById('btn-4').addEventListener('click', dispNum[4]);
-document.getElementById('btn-5').addEventListener('click', dispNum[5]);
-document.getElementById('btn-6').addEventListener('click', dispNum[6]);
-document.getElementById('btn-7').addEventListener('click', dispNum[7]);
-document.getElementById('btn-8').addEventListener('click', dispNum[8]);
-document.getElementById('btn-9').addEventListener('click', dispNum[9]);
+document.querySelector('.number-buttons').addEventListener('click', (event) => {
+  const { target } = event;
+  if (!target.matches('button')) {
+    return;
+  }
+
+  /*
+  if (target.classList.contains('decimal')) {
+    inputDecimal(target.value);
+    updateDisplay();
+    return;
+  } */
+
+  numberDisp(target.value);
+});
 
 //  ?For Number Manupulation
 document.getElementById('btn-int').addEventListener('click', flipNum);
